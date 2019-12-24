2c2051cca2616732ba3ed94d0353f439b29a0dd8
ghadd <danyatimachov@gmail.com>
2019-12-24 16:19:57 +0200

    added insert function

---
+++ LinkedList.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*A single element of Linked Structure*/
+typedef struct Node {
+	int value;
+	struct Node* next;
+	struct Node* prev;
+} Node;
+
+/*Linked list data itself*/
+typedef struct LinkedList {
+	Node* head;
+	Node* tail;
+	int size;
+} LinkedList;
+
+typedef unsigned int bool;
+#define true 1
+#define false 0
+
+/*in order to prevent from copying all the elements of structure (12 bytes) passing a pointer to use only 4 bytes xD*/
+void display(LinkedList* listPtr);
+void display_reverse(LinkedList* listPtr);
+
+bool is_empty(LinkedList* listPtr);
+
+void init(LinkedList** listPtr, int value);
+void push_back(LinkedList** listPtr, int value);
+void push_front(LinkedList** listPtr, int value);
+void insert_by_index(LinkedList** listPtr, int value, int index);
+//void insert_by_node(LinkedList* listPtr, int value, Node* prevNode);
+void delete_by_index(LinkedList** listPtr, int index);
+void delete_by_node(LinkedList** listPtr, Node* toDelete);
---
+++ Main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "LinkedList.h"
+
+int main(void) {
+	LinkedList* myList = NULL;
+
+	if (is_empty(myList)) {
+		printf("List is Empty\n");
+	}
+	init(&myList, 2);
+	delete_by_index(&myList, 0);
+
+	push_back(&myList, 3);
+	push_front(&myList, 1);
+	push_front(&myList, 0);
+
+	insert_by_index(&myList, 99, 0);
+
+
+	display(myList);
+
+	delete_by_index(&myList, 1);
+
+	display_reverse(myList);
+	if (!is_empty(myList)) {
+		printf("List is NOT Empty\n");
+	}
+	printf("Size: %d\n", myList->size);
+
+	return 0;
+}
